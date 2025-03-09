828c80af9053d3c314b78b0a422aa37618e5e105
Gabriele Fronzé <sucre.91@hotmail.it>
2025-03-09 16:50:32 +0100

    Test mermaid

--- assets/js/mermaid-init.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  mermaid.initialize({
-    startOnLoad: true,
-    theme: "default",
-  });
-  window.mermaid.init(undefined, document.querySelectorAll('.language-mermaid'));
-}); 
